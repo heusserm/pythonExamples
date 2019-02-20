c063ad1eefdb63ef2880862de0a6d19bee483604
heusserm <matt.heusser@gmail.com>
2019-02-20 15:05:21 -0500

    turned into a test

--- selenium/simpletest.py
+++ selenium/simpletest.py
@@ -1,12 +1,47 @@
+#Tested in python3
+#To run on OSX:
+#
+#
+# brew install python
+# pip3 install selenium
+#
+# 
+# To run:
+# python3 simpletest.py
+#
+
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get("https://www.saucelabs.com")
-element = driver.find_element(By.XPATH, '//a[text()="Platforms"]');
-element.click();
-pricing_link = driver.find_element(By.XPATH, '//a[text()="Pricing"]');
-pricing_link.click();
-driver.close();
+
+class WebDriverPythonBasics(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+
+
+    def test_saucelabs_homepage_header_displayed(self):
+        self.browser.get("https://www.saucelabs.com")
+        element = self.browser.find_element(By.XPATH, '//a[text()="Platforms"]');
+        self.assertTrue(element.is_displayed());
+        element.click();
+        pricing_link = self.browser.find_element(By.XPATH, '//a[text()="Pricing"]');
+        self.assertTrue(pricing_link.is_displayed());
+        pricing_link.click();
+
+
+
+
+    def tearDown(self):
+        self.browser.close()
+
+if __name__ == '__main__':
+        unittest.main()
+
+
+
+
 
 
